e6660002cb1b6bae2b49dc66a53278624e9ea411
Stephen Wu <wjwjq456@gmail.com>
2018-06-06 17:10:49 +0800

    initial commit

--- src/__test__/Hello.test.tsx
+++ src/__test__/Hello.test.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import * as enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-enzyme.configure({ adapter: new Adapter() });
 
 import Hello from '../components/Hello';
 
